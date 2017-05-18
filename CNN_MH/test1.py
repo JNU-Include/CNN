5ae69b306d803be952d238c90c706e1174865bb2
Lming1 <spear131121@gmail.com>
2017-05-18 23:36:07 +0900

    CNN Max_Pooling

--- CNN_MH/test1.py
+++ CNN_MH/test1.py
@@ -17,8 +17,8 @@ plt.imshow(image.reshape(3, 3), cmap='Greys')
 # print ("imag: \n", image)
 
 print("image.shape", image.shape)
-weight = tf.constant([[[[1.]], [[1.]]],
-                      [[[1.]], [[1.]]]])
+weight = tf.constant([[[[1.,10.,-1.]], [[1.,10.,-1.]]],
+                      [[[1.,10.,-1.]], [[1.,10.,-1.]]]])
 print("weight.shape", weight.shape)
 conv2d = tf.nn.conv2d(image, weight, strides = [1, 1, 1, 1], padding='SAME')
 conv2d_img = conv2d.eval()
@@ -26,4 +26,4 @@ print("conv2d_img.shape", conv2d_img.shape)
 conv2d_img = np.swapaxes(conv2d_img, 0, 3)
 for i, one_img in enumerate(conv2d_img):
     print(one_img.reshape(3,3))
-    plt.subplot(1,2,i+1), plt.imshow(one_img.reshape(3,3), cmap='gray')
+    plt.subplot(1,3,i+1), plt.imshow(one_img.reshape(3,3), cmap='gray')
