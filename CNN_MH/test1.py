f71efc7f3686fd2d5ecf29c75d1b947e56f3332b
Lming1 <spear131121@gmail.com>
2017-05-18 15:31:32 +0900

    cnn study

--- CNN_MH/test1.py
+++ CNN_MH/test1.py
@@ -1,3 +1,14 @@
+import os
+# 경고 메시지 출력 여부... 1 - 항상 출력, 2 - 무시
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
 sess = tf.InteractiveSession()
+image = np.array([[[[1],[2],[3]],
+                   [[4],[5],[6]],
+                   [[7],[8],[9]]]], dtype=np.float32)
+print(image.shape)
+plt.imshow(image.reshape(3, 3), cmap='Greys')
